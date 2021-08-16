535d6b9a423c3f40337325ad3e36b65aa34f1a29
mayavathymahesan <mayavathymahesan@gmail.com>
2021-08-16 14:56:52 +0100

    day3 problem one EXPORT_SYMBOL

---
+++ d3p1_exportsym/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/device_drivers/native/hello_module/d3p1_exportsym/Module.symvers := sed 's/ko$$/o/' /home/pi/device_drivers/native/hello_module/d3p1_exportsym/modules.order | scripts/mod/modpost -m -a   -o /home/pi/device_drivers/native/hello_module/d3p1_exportsym/Module.symvers -e -i Module.symvers   -T -
