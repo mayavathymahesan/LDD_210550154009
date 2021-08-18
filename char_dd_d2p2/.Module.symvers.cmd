e001893c7a9ef3e4a9f7e4555ffe8900e8e362d2
mayavathymahesan@gmail.com <mayavathymahesan>
2021-08-18 05:27:25 +0100

    day4 OPEN READ WRITE CALL

---
+++ char_dd_d2p2/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/device_drivers/native/hello_module/char_dd_d2p2/Module.symvers := sed 's/ko$$/o/' /home/pi/device_drivers/native/hello_module/char_dd_d2p2/modules.order | scripts/mod/modpost -m -a   -o /home/pi/device_drivers/native/hello_module/char_dd_d2p2/Module.symvers -e -i Module.symvers   -T -
---
+++ char_dd_d2p2/chardriver.mod
@@ -0,0 +1,2 @@
+/home/pi/device_drivers/native/hello_module/char_dd_d2p2/chardriver.o
+
